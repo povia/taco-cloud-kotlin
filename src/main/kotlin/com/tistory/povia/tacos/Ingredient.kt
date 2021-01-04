3ff428e1ff0d2fb21b6180b541892fe1fa3df699
povia <tjddn020@naver.com>
2021-01-04 17:39:26 +0900

    building data classes

---
+++ src/main/kotlin/com/tistory/povia/tacos/Ingredient.kt
@@ -0,0 +1,7 @@
+package com.tistory.povia.tacos
+
+data class Ingredient(val id:String, val name:String, val type:Type)
+
+enum class Type{
+    WRAP, PROTEIN, VEGGIES, CHEESE, SAUSE
+}
---
+++ src/main/kotlin/com/tistory/povia/tacos/Taco.kt
@@ -0,0 +1,3 @@
+package com.tistory.povia.tacos
+
+data class Taco(val name:String?=null, val ingredients:List<Ingredient>?=null)
